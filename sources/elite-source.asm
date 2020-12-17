0993f1ac6f271badd4769414419ead0ab4f50fdf
Mark Moxon <mark@moxon.net>
2020-12-17 09:31:40 +0000

    System searching documented

--- sources/elite-source.asm
+++ sources/elite-source.asm
@@ -10523,62 +10523,131 @@ DTW7 = MT16 + 1         \ Point DTW7 to the second byte of the instruction above
                         \ call
 
 \ ******************************************************************************
+\
 \       Name: HME2
+\       Type: Subroutine
+\   Category: Charts
+\    Summary: Search the galaxy for a system
+\
 \ ******************************************************************************
 
 .HME2
 
- LDA #CYAN
- JSR DOCOL
- LDA #14
- JSR DETOK
- JSR TT103
- JSR TT81
- LDA #0
- STA XX20
+ LDA #CYAN              \ Send a #SETCOL CYAN command to the I/O processor to
+ JSR DOCOL              \ switch to colour 3, which is white in the chart view
+
+ LDA #14                \ Print extended token 14 ("{clear bottom of screen}
+ JSR DETOK              \ PLANET NAME?{fetch line input from keyboard}"). The
+                        \ last token calls MT26, which puts the entered search
+                        \ term in INWK+5 and the term length in Y
+
+ JSR TT103              \ Draw small crosshairs at coordinates (QQ9, QQ10),
+                        \ which will erase the crosshairs currently there
+
+ JSR TT81               \ Set the seeds in QQ15 (the selected system) to those
+                        \ of system 0 in the current galaxy (i.e. copy the seeds
+                        \ from QQ21 to QQ15)
+
+ LDA #0                 \ We now loop through the galaxy's systems in order,
+ STA XX20               \ until we find a match, so set XX20 to act as a system
+                        \ counter, starting with system 0
 
 .HME3
 
- JSR MT14
- JSR cpl
- LDX DTW5
- LDA INWK+5,X
- CMP #13
- BNE HME6
+ JSR MT14               \ Switch to justified text when printing extended
+                        \ tokens, so the call to cpl prints into the justified
+                        \ text buffer at BUF instead of the screen, and DTW5
+                        \ gets set to the length of the system name
+
+ JSR cpl                \ Print the selected system name into the justified text
+                        \ buffer
+
+ LDX DTW5               \ Fetch DTW5 into X, so X is now equal to the length of
+                        \ the selected system name
+
+ LDA INWK+5,X           \ Fetch the X-th character from the entered search term
+
+ CMP #13                \ If the X-th character is not a carriage return, then
+ BNE HME6               \ the selected system name and the entered search term
+                        \ are different lengths, so jump to HME6 to move on to
+                        \ the next system
 
 .HME4
 
- DEX
- LDA INWK+5,X
- ORA #32
- CMP BUF,X
- BEQ HME4
- TXA
- BMI HME5
+ DEX                    \ Decrement X so it points to the last letter of the
+                        \ selected system name (and, when we loop back here, it
+                        \ points to the next letter to the left)
+
+ LDA INWK+5,X           \ Set A to the X-th character of the entered search term
+
+ ORA #%00100000         \ Set bit 5 of the character to make it lower case
+
+ CMP BUF,X              \ If the character in A matches the X-th character of
+ BEQ HME4               \ the selected system name in BUF, loop back to HME4 to
+                        \ check the next letter to the left
+
+ TXA                    \ The last comparison didn't match, so copy the letter
+ BMI HME5               \ number into A, and if it's negative, that means we
+                        \ managed to go past the first letters of each term
+                        \ before we failed to get a match, so the terms are the
+                        \ same, so jump to HME5 to process a successful search
 
 .HME6
 
- JSR TT20
- INC XX20
- BNE HME3
- JSR TT111
- JSR TT103
- LDA #40
- JSR NOISE
- LDA #215
- JMP DETOK
-\Not found
+                        \ If we get here then the selected system name and the
+                        \ entered search term did not match
+
+ JSR TT20               \ We want to move on to the next system, so call TT20
+                        \ to twist the three 16-bit seeds in QQ15
+
+ INC XX20               \ Incrememt the system counter in XX20
+
+ BNE HME3               \ If we haven't yet checked all 256 systems in the
+                        \ current galaxy, loop back to HME3 to check the next
+                        \ system
+
+                        \ If we get here then the entered search term did not
+                        \ match any systems in the current galaxy
+
+ JSR TT111              \ Select the system closest to galactic coordinates
+                        \ (QQ9, QQ10), so we can put the crosshairs back where
+                        \ they were before the search
+
+ JSR TT103              \ Draw small crosshairs at coordinates (QQ9, QQ10)
+
+ LDA #40                \ Call the NOISE routine with A = 40 to make a low,
+ JSR NOISE              \ long beep to indicate a failed search
+
+ LDA #215               \ Print extended token 215 ("{left align} UNKNOWN
+ JMP DETOK              \ PLANET"), which will print on-screem as the left align
+                        \ code disables justified text, and return from the
+                        \ subroutine using a tail call
 
 .HME5
 
- LDA QQ15+3
- STA QQ9
- LDA QQ15+1
- STA QQ10
- JSR TT111
- JSR TT103
- JSR MT15
- JMP T95
+                        \ If we get here then we have found a match for the
+                        \ entered search
+
+ LDA QQ15+3             \ The x-coordinate of the system described by the seeds
+ STA QQ9                \ in QQ15 is in QQ15+3 (w1_hi), so we copy this to QQ9
+                        \ as the x-coordinate of the search result
+
+ LDA QQ15+1             \ The y-coordinate of the system described by the seeds
+ STA QQ10               \ in QQ15 is in QQ15+1 (w0_hi), so we copy this to QQ10
+                        \ as the y-coordinate of the search result
+
+ JSR TT111              \ Select the system closest to galactic coordinates
+                        \ (QQ9, QQ10)
+
+ JSR TT103              \ Draw small crosshairs at coordinates (QQ9, QQ10)
+
+ JSR MT15               \ Switch to left-aligned text when printing extended
+                        \ tokens so future tokens will print to the screen (as
+                        \ this disables justified text)
+
+ JMP T95                \ Jump to T95 to print the distance to the selected
+                        \ system and return from the subroutine using a tail
+                        \ call
 
 \ ******************************************************************************
 \
@@ -27858,6 +27927,10 @@ LOAD_F% = LOAD% + P% - CODE%
 \   X                   The amount to move the crosshairs in the x-axis
 \
 \   Y                   The amount to move the crosshairs in the y-axis
+\
+\ Other entry points:
+\
+\   T95                 Print the distance to the selected system
 \
 \ ******************************************************************************
 
