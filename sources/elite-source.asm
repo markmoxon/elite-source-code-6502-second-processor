a5737388921c469604d0b024d5c41492cfc4d966
Mark Moxon <mark@moxon.net>
2020-12-21 13:33:15 +0000

    Finished mission commentary

--- sources/elite-source.asm
+++ sources/elite-source.asm
@@ -15915,97 +15915,186 @@ LOAD_C% = LOAD% +P% - CODE%
 \       Name: BRIEF
 \       Type: Subroutine
 \   Category: Missions
-\    Summary: Start mission 1
+\    Summary: Start mission 1 and show the mission briefing
 \
 \ ------------------------------------------------------------------------------
 \
+\ This routine does the following:
 \
+\   * Clear the screen
+\   * Display "INCOMING MESSAGE" in the middle of the screen
+\   * Wait for 2 seconds
+\   * Clear the screen
+\   * Show the Constrictor rolling and pitching in the middle of the screen
+\   * Do this for 64 loop iterations
+\   * Move the ship away from us and up until it's near the top of the screen
+\   * Show the mission 1 briefing in extended token 10
+\
+\ The mission briefing ends with a "{display ship, wait for key press}" token,
+\ which calls the PAUSE routine. This continues to display the rotating ship,
+\ waiting until a key is pressed, and then removes the ship from the screen.
 \
 \ ******************************************************************************
 
 .BRIEF
 
- LSR TP                 \ Set bit 0 of TP to indicate mission 1 is in progress
- SEC
+ LSR TP                 \ Set bit 0 of TP to indicate that mission 1 is now in
+ SEC                    \ progress
  ROL TP
 
- JSR BRIS
+ JSR BRIS               \ Call BRIS to clear the screen, display "INCOMING
+                        \ MESSAGE" and wait for 2 seconds
 
  JSR ZINF               \ Call ZINF to reset the INWK ship workspace
 
- LDA #CON
+ LDA #CON               \ Set the ship type in TYPE to the Constrictor
  STA TYPE
- JSR NWSHP
- LDA #1
+
+ JSR NWSHP              \ Add a new Constrictor to the local bubble (in this
+                        \ case, the briefing screen)
+
+ LDA #1                 \ Move the text cursor to column 1
  JSR DOXC
- STA INWK+7
- JSR TT66
- LDA #64
+
+ STA INWK+7             \ Set z_hi = 1, the distance at which we show the
+                        \ rotating ship
+
+ JSR TT66               \ Clear the top part of the screen, draw a white border,
+                        \ and set the current view type in QQ11 to 1
+
+ LDA #64                \ Set the main loop counter to 64, so the ship rotates
+                        \ for 64 iterations through MVEIT
  STA MCNT
 
 .BRL1
 
- LDX #127
- STX INWK+29
- STX INWK+30
- JSR LL9
- JSR MVEIT
- DEC MCNT
- BNE BRL1
+ LDX #%01111111         \ Set the ship's roll counter to a positive roll that
+ STX INWK+29            \ doesn't dampen
+
+ STX INWK+30            \ Set the ship's pitch counter to a positive pitch that
+                        \ doesn't dampen
+
+ JSR LL9                \ Draw the ship on screen
+
+ JSR MVEIT              \ Call MVEIT to rotate the ship in space
+
+ DEC MCNT               \ Decrease the counter in MCNT
+
+ BNE BRL1               \ Loop back to keep moving the ship until we have done
+                        \ all 64 iterations
 
 .BRL2
 
- LSR INWK
- INC INWK+6
- BEQ BR2
- INC INWK+6
- BEQ BR2
- LDX INWK+3
+ LSR INWK               \ Halve x_lo so the Constrictor moves towards the centre
+ 
+ INC INWK+6             \ Increment z_lo so the Constrictor moves away from us
+
+ BEQ BR2                \ If z_lo = 0 (i.e. it just went past 255), jump to BR2
+                        \ to show the briefing
+
+ INC INWK+6             \ Increment z_lo so the Constrictor moves a bit further
+                        \ away from us
+
+ BEQ BR2                \ If z_lo = 0 (i.e. it just went past 255), jump out of
+                        \ the loop to BR2 to stop moving the ship up the screen
+                        \ and show the briefing
+
+ LDX INWK+3             \ Set X = y_lo + 1
  INX
- CPX #112
+
+ CPX #112               \ If X < 112 then skip the next instruction
  BCC P%+4
- LDX #112
- STX INWK+3
- JSR LL9
- JSR MVEIT
- JMP BRL2
+
+ LDX #112               \ X is bigger than 112, so set X = 112 so that X has a
+                        \ maximum value of 112
+
+ STX INWK+3             \ Set y_lo = X
+                        \          = y_lo + 1
+                        \
+                        \ so the ship moves up the screen (as space coordinates
+                        \ have the y-axis going up)
+
+ JSR LL9                \ Draw the ship on screen
+
+ JSR MVEIT              \ Call MVEIT to move and rotate the ship in space
+
+ JMP BRL2               \ Loop back to keep moving the ship up the screen and
+                        \ away from us
 
 .BR2
 
- INC INWK+7
- LDA #10
- BNE BRPS
+ INC INWK+7             \ Increment z_hi, to keep the ship at the same distance
+                        \ as we just incremented z_lo past 255
+
+ LDA #10                \ Set A = 10 so the call to BRP prints extended token 10
+                        \ (the briefing for mission 1 where we find out all
+                        \ about the stolen Constrictor)
+
+ BNE BRPS               \ Jump to BRP via BRPS to print the extended token in A
+                        \ and show the Status Mode screen), returning from the
+                        \ subroutine using a tail call (this BNE is effectively
+                        \ a JMP as A is never zero)
+
+\ ******************************************************************************
+\
+\       Name: BRIS
+\       Type: Subroutine
+\   Category: Missions
+\    Summary: Clear the screen, display "INCOMING MESSAGE" and wait for 2
+\             seconds
+\
+\ ******************************************************************************
 
 .BRIS
 
- LDA #216
- JSR DETOK
- LDY #100
- JMP DELAY
+ LDA #216               \ Print extended token 216 ("{clear screen}{tab 6}{move
+ JSR DETOK              \ to row 10, white, lower case}{white}{all caps}INCOMING
+                        \ MESSAGE"
+
+ LDY #100               \ Delay for 100 vertical syncs (100/50 = 2 seconds) and
+ JMP DELAY              \ return from the subroutine using a tail call
 
 \ ******************************************************************************
 \
 \       Name: PAUSE
 \       Type: Subroutine
-\   Category: Missions
-\    Summary: 
+\   Category: Keyboard
+\    Summary: Display a rotating ship, waiting until a key is pressed, then
+\             remove the ship from the screen
 \
 \ ******************************************************************************
 
 .PAUSE
 
- JSR PAS1
- BNE PAUSE
+ JSR PAS1               \ Call PAS1 to display the rotating ship at space
+                        \ coordinates (0, 112, 256) and scan the keyboard,
+                        \ returning the internal key number in X (or 0 for no
+                        \ key press)
+
+ BNE PAUSE              \ If a key was already being held down when we entered
+                        \ this routine, keep looping back up to PAUSE, until
+                        \ the key is released
 
 .PAL1
 
- JSR PAS1
- BEQ PAL1
- LDA #0
- STA INWK+31
- LDA #1
- JSR TT66
- JSR LL9
+ JSR PAS1               \ Call PAS1 to display the rotating ship at space
+                        \ coordinates (0, 112, 256) and scan the keyboard,
+                        \ returning the internal key number in X (or 0 for no
+                        \ key press)
+
+ BEQ PAL1               \ Keep looping up to PAL1 until a key is pressed
+
+ LDA #0                 \ Set the ship's AI flag to 0 (no AI) so it doesn't get
+ STA INWK+31            \ any ideas of its pwn
+
+ LDA #1                 \ Clear the top part of the screen, draw a white border,
+ JSR TT66               \ and set the current view type in QQ11 to 1
+
+ JSR LL9                \ Draw the ship on screen to remove it
+
+                        \ Fall through into MT23 to move to row 10, switch to
+                        \ white text, and switch to lower case when printing
+                        \ extended tokens
 
 \ ******************************************************************************
 \
@@ -16062,27 +16151,37 @@ LOAD_C% = LOAD% +P% - CODE%
 \
 \       Name: PAS1
 \       Type: Subroutine
-\   Category: Missions
-\    Summary: 
+\   Category: Keyboard
+\    Summary: Display a rotating ship at space coordinates (0, 112, 256) and
+\             scan the keyboard
+\
+\ ------------------------------------------------------------------------------
+\
+\ Returns:
+\
+\   X                   If a key is being pressed, X contains the internal key
+\                       number, otherwise it contains 0
+\
+\   A                   Contains the same as X
 \
 \ ******************************************************************************
 
 .PAS1
 
- LDA #112
+ LDA #112               \ Set y_lo = 112
  STA INWK+3
 
- LDA #0
+ LDA #0                 \ Set x_lo = 0
  STA INWK
 
- STA INWK+6
+ STA INWK+6             \ Set z_lo = 0
 
- LDA #2
+ LDA #2                 \ Set z_hi = 1, so (z_hi z_lo) = 256
  STA INWK+7
 
- JSR LL9
+ JSR LL9                \ Draw the ship on screen
 
- JSR MVEIT
+ JSR MVEIT              \ Call MVEIT to move and rotate the ship in space
 
  JMP RDKEY              \ Scan the keyboard for a key press and return the
                         \ internal key number in X (or 0 for no key press),
@@ -16092,8 +16191,8 @@ LOAD_C% = LOAD% +P% - CODE%
 \
 \       Name: PAUSE2
 \       Type: Subroutine
-\   Category: Missions
-\    Summary: 
+\   Category: Keyboard
+\    Summary: Wait until a key is pressed, ignoring any existing key press
 \
 \ ******************************************************************************
 
@@ -38734,9 +38833,9 @@ LOAD_H% = LOAD% + P% - CODE%
 
 \ ******************************************************************************
 \
-\       Name: 
+\       Name: Checksum
 \       Type: Subroutine
-\   Category: Save and load
+\   Category: Copy protection
 \    Summary: 
 \
 \ ******************************************************************************
@@ -38747,7 +38846,7 @@ LOAD_H% = LOAD% + P% - CODE%
  LDY #0
  STY V
  LDX #&10
- LDA (SC)\++RH
+ LDA (SC)
  TXA
 
 .CHKLoop
