783ea025b3f65682b7a8d3b7ef4dacdd05784f26
Mark Moxon <mark@moxon.net>
2023-04-26 15:30:12 +0100

    Fixed minor typo in RUGAL commentary

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -47201,8 +47201,8 @@ ENDIF
 \ the table below).
 \
 \ In other words, Teorge, Arredi, Anreer and Lave have extended decription
-\ overrides descriptions that are always shown, while the rest only appear when
-\ mission 1 is in progress.
+\ overrides that are always shown, while the rest only appear when mission 1 is
+\ in progress.
 \
 \ The three variables work as follows:
 \
