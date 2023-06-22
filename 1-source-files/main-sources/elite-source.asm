01e496e25ffed0c3a329513398ed30e52c13dd91
Mark Moxon <mark@moxon.net>
2023-06-22 16:58:50 +0100

    Minor corrections to the commentary

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -24920,8 +24920,8 @@ ENDIF
 \
 \ ------------------------------------------------------------------------------
 \
-\ Calculate the following, where A is a signed 8-bit integer and the result is a
-\ signed 16-bit integer:
+\ Calculate the following, where A is a sign-magnitude 8-bit integer and the
+\ result is a signed 16-bit integer:
 \
 \   (Y X) = A / 10
 \
@@ -25062,7 +25062,7 @@ ENDIF
                         \ +96 as the vector has been normalised
 
  JSR SPS2               \ Set (Y X) = A / 10, so X will be from -9 to +9, which
-                        \ is the y-offset from the centre of the compass of the
+                        \ is the x-offset from the centre of the compass of the
                         \ dot we want to draw. Returns with the C flag clear
 
  STX T                  \ Set COMY = 204 - X, as 203 is the pixel y-coordinate
@@ -29470,7 +29470,7 @@ ENDIF
 .ss
 
  CPX NOMSL              \ If the counter is equal to the number of missiles,
- BEQ SAL8               \ jump down to SQL8 to draw remaining the missiles, as
+ BEQ SAL8               \ jump down to SAL8 to draw the remaining missiles, as
                         \ the rest of them are present and should be drawn in
                         \ green
 
@@ -33411,7 +33411,7 @@ ENDIF
 \
 \       Name: EXNO2
 \       Type: Subroutine
-\   Category: Sound
+\   Category: Status
 \    Summary: Process us making a kill
 \  Deep dive: Combat rank
 \
@@ -39961,8 +39961,9 @@ ENDIF
 
  LDA #0                 \ Set A to 0 to stop the speed from going negative
 
- LDY #15                \ Fetch byte #15 from the ship's blueprint, which
-                        \ contains the ship's maximum speed
+ LDY #15                \ We now fetch byte #15 from the ship's blueprint, which
+                        \ contains the ship's maximum speed, so set Y = 15 to
+                        \ use as an index
 
  CMP (XX0),Y            \ If A < the ship's maximum speed, skip the following
  BCC P%+4               \ instruction
