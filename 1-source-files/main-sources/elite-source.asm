3c3d1001bcc4e12c8bf44fb0d16211618d684ef3
Mark Moxon <mark@moxon.net>
2023-10-07 16:26:00 +0100

    Merge Inventory category into Market, recategorise various routines

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -19713,7 +19713,7 @@ ENDIF
 \
 \       Name: TT210
 \       Type: Subroutine
-\   Category: Inventory
+\   Category: Market
 \    Summary: Show a list of current cargo in our hold, optionally to sell
 \
 \ ------------------------------------------------------------------------------
@@ -19876,7 +19876,7 @@ ENDIF
 \
 \       Name: TT213
 \       Type: Subroutine
-\   Category: Inventory
+\   Category: Market
 \    Summary: Show the Inventory screen (red key f9)
 \
 \ ******************************************************************************
@@ -19915,7 +19915,7 @@ ENDIF
 \
 \       Name: TT214
 \       Type: Subroutine
-\   Category: Inventory
+\   Category: Keyboard
 \    Summary: Ask a question with a "Y/N?" prompt and return the response
 \
 \ ------------------------------------------------------------------------------
@@ -29599,7 +29599,7 @@ ENDIF
 \
 \       Name: DORND
 \       Type: Subroutine
-\   Category: Utility routines
+\   Category: Maths (Arithmetic)
 \    Summary: Generate random numbers
 \  Deep dive: Generating random numbers
 \             Fixing ship positions
