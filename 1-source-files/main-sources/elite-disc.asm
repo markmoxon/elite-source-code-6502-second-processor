6151c014e5fdc15b9b67aed5ca5048796c9944b4
Mark Moxon <mark@moxon.net>
2024-08-20 13:30:25 +0100

    Add asteroid spiral universe file

--- 1-source-files/main-sources/elite-disc.asm
+++ 1-source-files/main-sources/elite-disc.asm
@@ -81,4 +81,5 @@ ENDIF
  PUTFILE "1-source-files/universe-files/U.SHIPID6.bin", "U.SHIPID6", &000000, &000000
  PUTFILE "1-source-files/universe-files/U.VIPERS.bin", "U.VIPERS", &000000, &000000
  PUTFILE "1-source-files/universe-files/U.SPIRAL.bin", "U.SPIRAL", &000000, &000000
+ PUTFILE "1-source-files/universe-files/U.SPIRAL2.bin", "U.SPIRAL2", &000000, &000000
 
