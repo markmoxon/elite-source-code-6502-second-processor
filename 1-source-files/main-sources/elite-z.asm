6c3b248217601c7850e91fc1adca2cf812c7daea
Mark Moxon <mark@moxon.net>
2025-01-09 16:56:20 +0000

    Clearer commentary around div 8 calculation for finding character row

--- 1-source-files/main-sources/elite-z.asm
+++ 1-source-files/main-sources/elite-z.asm
@@ -5238,7 +5238,7 @@ ENDIF
  LDA (OSSC),Y           \ Set P to byte #2 from the Y-th pixel block in OSSC,
  STA P                  \ which contains the point's distance value (ZZ)
 
- AND #%00000111         \ If ZZ is a multiple of 8 (which will be the case for
+ AND #7                 \ If ZZ is a multiple of 8 (which will be the case for
  BEQ PX5                \ pixels sent by the parasite's PIXEL routine), jump to
                         \ PX5 to draw stardust particles and dots on the
                         \ Long-range Chart
@@ -5291,9 +5291,9 @@ ENDIF
                         \ the first memory page for the row... so we need to
                         \ increment SC+1 to point to the correct page
 
- TYA                    \ Set Y to just bits 0-2 of the y-coordinate, which will
- AND #%00000111         \ be the number of the pixel row we need to draw into
- TAY                    \ within the character block
+ TYA                    \ Set Y = Y mod 8, which is the pixel row within the
+ AND #7                 \ character block at which we want to draw the pixel
+ TAY                    \ (as each character block has 8 rows)
 
  TXA                    \ Copy bits 0-1 of the x-coordinate to bits 0-1 of X,
  AND #%00000011         \ which will now be in the range 0-3, and will contain
@@ -5444,9 +5444,9 @@ ENDIF
                         \ the first memory page for the row... so we need to
                         \ increment SC+1 to point to the correct page
 
- TYA                    \ Set Y to just bits 0-2 of the y-coordinate, which will
- AND #%00000111         \ be the number of the pixel row we need to draw into
- TAY                    \ within the character block
+ TYA                    \ Set Y = Y mod 8, which is the pixel row within the
+ AND #7                 \ character block at which we want to draw the start of
+ TAY                    \ our line (as each character block has 8 rows)
 
  TXA                    \ Copy bits 0-1 of the x-coordinate to bits 0-1 of X,
  AND #%00000011         \ which will now be in the range 0-3, and will contain
