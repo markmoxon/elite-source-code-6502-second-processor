8dd9dd0fe892b8cb3f84d96148dbc488c0f4a43c
Mark Moxon <mark@moxon.net>
2025-03-04 12:13:19 +0000

    Fix incorrect comment

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -54858,8 +54858,8 @@ ENDMACRO
 
 .WaitForSync
 
- LDA #1                 \ Set the parameter to zero to restart the sync counter
- STA musicBuff+2
+ LDA #1                 \ Set the parameter to 1 to wait for the sync counter to
+ STA musicBuff+2        \ count down
 
  LDX #LO(musicBuff)     \ Set (Y X) to point to the musicBuff parameter
  LDY #HI(musicBuff)     \ block
