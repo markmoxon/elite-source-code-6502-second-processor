c86780ff775730405d652e0493c13fa03520eca5
Mark Moxon <mark@moxon.net>
2026-02-20 12:25:34 +0000

    Clarify that the random numbers in PLL1 are signed

--- 1-source-files/main-sources/elite-loader1.asm
+++ 1-source-files/main-sources/elite-loader1.asm
@@ -617,7 +617,8 @@ ENDIF
                         \ main game code's random seeds in RAND (so this seeds
                         \ the random number generator)
 
- JSR DORND              \ Set A and X to random numbers, say A = r1
+ JSR DORND              \ Set A and X to signed random numbers between -128 and
+                        \ 127, so let's say A = r1
 
  JSR SQUA2              \ Set (A P) = A * A
                         \           = r1^2
@@ -626,7 +627,8 @@ ENDIF
  LDA P                  \             = r1^2
  STA ZP
 
- JSR DORND              \ Set A and X to random numbers, say A = r2
+ JSR DORND              \ Set A and X to signed random numbers between -128 and
+                        \ 127, so let's say A = r2
 
  STA YY                 \ Set YY = A
                         \        = r2
@@ -733,7 +735,7 @@ ENDIF
 .PLL2
 
  JSR DORND              \ Set A and X to signed random numbers between -128 and
-                        \ 127, say A = r3
+                        \ 127, so let's say A = r3
 
  TAX                    \ Set X = A
                         \       = r3
@@ -745,7 +747,7 @@ ENDIF
                         \          = r3^2 / 256
 
  JSR DORND              \ Set A and X to signed random numbers between -128 and
-                        \ 127, say A = r4
+                        \ 127, so let's say A = r4
 
  STA YY                 \ Set YY = r4
 
@@ -796,7 +798,8 @@ ENDIF
 
 .PLL3
 
- JSR DORND              \ Set A and X to random numbers, say A = r5
+ JSR DORND              \ Set A and X to signed random numbers between -128 and
+                        \ 127, so let's say A = r5
 
  STA ZP                 \ Set ZP = r5
 
@@ -806,7 +809,8 @@ ENDIF
  STA ZP+1               \ Set ZP+1 = A
                         \          = r5^2 / 256
 
- JSR DORND              \ Set A and X to random numbers, say A = r6
+ JSR DORND              \ Set A and X to signed random numbers between -128 and
+                        \ 127, so let's say A = r6
 
  STA YY                 \ Set YY = r6
 
